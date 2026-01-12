097f2199d70888b7a999add24d601105fb97a4f9
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2026-01-12 11:41:59 +0900

    Add health check API endpoint

--- wordbook/urls.py
+++ wordbook/urls.py
@@ -3,17 +3,24 @@ from django.contrib import admin
 from django.urls import path, include
 from error.views import custom_404, custom_500
 from . import views
+from django.http import JsonResponse
 
 # カスタムエラーハンドラ
 handler404 = custom_404
 handler500 = custom_500
 
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home , name="home"),
-    path('accounts/', include('accounts.urls')),
-    path('contact/', include('contact.urls')),
-    path('dictionary/', include('dictionary.urls')),
-    path('flashcard/', include('flashcard.urls')),
-    path('test-error/', views.test_error, name='test_error'), # ERRORログ用
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("accounts/", include("accounts.urls")),
+    path("contact/", include("contact.urls")),
+    path("dictionary/", include("dictionary.urls")),
+    path("flashcard/", include("flashcard.urls")),
+    path("test-error/", views.test_error, name="test_error"),  # ERRORログ用
+    path("api/health", health_check),
 ]
